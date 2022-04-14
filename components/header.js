8c1b0612c59212ea4de2ca0209868bf1feff8185
Axel Julián Suarez <53792359+lechuza2013@users.noreply.github.com>
2022-04-14 16:42:13 -0300

    Pequeño cambio en la ruta de los demás htmls

--- components/header.js
+++ components/header.js
@@ -6,9 +6,9 @@ function createHeaderComponent(el){
         <h2 class="header__title">AXEL</h2>
         <a>
         <nav class="header__nav">
-            <a class="header__nav-links" href="../portfolio.html">Portfolio</a>
-            <a class="header__nav-links" href="../servicios.html">Servicios</a>
-            <a class="header__nav-links" href="../contacto.html">Contacto</a>
+            <a class="header__nav-links" href="./portfolio.html">Portfolio</a>
+            <a class="header__nav-links" href="./servicios.html">Servicios</a>
+            <a class="header__nav-links" href="./contacto.html">Contacto</a>
         </nav>
         <div class="header__menu">
             <div class="header__menu-raya"></div>
@@ -20,9 +20,9 @@ function createHeaderComponent(el){
 
         <button class="header__window-button">X</button>
 
-        <a class="header__window-links" href="../portfolio.html">Portfolio</a>
-        <a class="header__window-links" href="../servicios.html">Servicios</a>
-        <a class="header__window-links" href="../contacto.html">Contacto</a>
+        <a class="header__window-links" href="./portfolio.html">Portfolio</a>
+        <a class="header__window-links" href="./servicios.html">Servicios</a>
+        <a class="header__window-links" href="./contacto.html">Contacto</a>
     </div>`;
 
    el.appendChild(componentEl);
