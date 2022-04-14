5cb751f26b403f90fd7f4dd74b009986f55772bc
Axel Julián Suarez <53792359+lechuza2013@users.noreply.github.com>
2022-04-14 16:39:57 -0300

    Pequeño cambio a la ruta para los iconos

--- components/footer.js
+++ components/footer.js
@@ -10,19 +10,19 @@ function createFooterComponent(el){
         <div class="footer__container-social-media">
             <a href="https://www.instagram.com/axeljsu/" target="_blank">
                 <label for="icon-ig" class="footer__labels">Instagram</label>
-                <img id="icon-ig" class="footer__icon" src="../images/icon-instagram.png">
+                <img id="icon-ig" class="footer__icon" src="./images/icon-instagram.png">
             </a>
         </div>
         <div class="footer__container-social-media">
             <a href="https://github.com/lechuza2013" target="_blank">
                 <label for="icon-git" class="footer__labels">Github</label>
-                <img id="icon-git" class="footer__icon" src="../images/icon-github.png">
+                <img id="icon-git" class="footer__icon" src="./images/icon-github.png">
             </a>
         </div>
         <div class="footer__container-social-media">
             <a href="https://twitter.com/Axeloide9" target="_blank">
                 <label for="icon-twitter" class="footer__labels">Twitter</label>
-                <img id="icon-twitter" class="footer__icon" src="../images/icon-twitter.png">
+                <img id="icon-twitter" class="footer__icon" src="./images/icon-twitter.png">
             </a>
         </div>
     </div>
